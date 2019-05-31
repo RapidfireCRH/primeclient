ebc833bdf6a8d87b5cd39aad22dff4bb5a42ed30
RapidfireCRH <crhrapidfire@gmail.com>
2019-05-31 13:44:29 +0000

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,47 @@
+import mysql.connector
+
 #Details for db login
 q_username = ''
 q_password = ''
 q_host = ''
 q_database = ''
 
-checkoutnum = 1000
+def query(sql):
+	try:
+		conn = mysql.connector.connect(user=q_username, password=q_password, host=q_host, database=q_database)
+		cur = conn.cursor()
+		cur.execute(sql)
+		return cur.fetchall()
+	except mysql.connector.Error as error :
+		conn.rollback() #rollback if any exception occured
+		print("Failed calling record. {}".format(error))
+	
+def insert(sql, val):
+	try:
+		conn = mysql.connector.connect(user=q_username, password=q_password, host=q_host, database=q_database)
+		cur = conn.cursor()
+		cur.execute(sql, val)
+		conn.commit()
+	except mysql.connector.Error as error :
+		conn.rollback() #rollback if any exception occured
+		print("Failed inserting record. {}".format(error))
+		
+def delete(sql):
+	try:
+		conn = mysql.connector.connect(user=q_username, password=q_password, host=q_host, database=q_database)
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+	except mysql.connector.Error as error :
+		conn.rollback() #rollback if any exception occured
+		print("Failed deleting record. {}".format(error))
+
+def update(sql):
+	try:
+		conn = mysql.connector.connect(user=q_username, password=q_password, host=q_host, database=q_database)
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+	except mysql.connector.Error as error :
+		conn.rollback() #rollback if any exception occured
+		print("Failed updating record. {}".format(error))
